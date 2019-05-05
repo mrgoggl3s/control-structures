d023da194f5532458bff96a3509e0fa3e9a21aae
mrgoggl3s <razvan.jijau@gmail.com>
2019-05-05 15:52:35 +0300

    Ex6

---
+++ src/Ex6.java
@@ -0,0 +1,21 @@
+public class Ex6 {
+    public static void main(String[] args) {
+
+        int primeleNrImpare = 7;
+        int counter = 0;
+        int nrInteres = 0;
+
+        while (counter < primeleNrImpare){
+            if(nrInteres % 2 !=0 ){
+                System.out.println(nrInteres);
+                counter++;
+
+            }
+            nrInteres++;
+        }
+
+
+
+    }
+}
+
