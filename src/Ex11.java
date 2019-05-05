b7cae6c556f25607c90197091c6e6604df4c7ec1
mrgoggl3s <razvan.jijau@gmail.com>
2019-05-05 17:29:36 +0300

    Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,51 @@
+public class Ex11 {
+    public static void main(String[] args) {
+        int n = 3;
+//        if(n>0 && n<9) {
+//            System.out.println("numarul este " + n);
+//        }
+//        else{
+//            System.out.println("numarul este grsit");
+//
+//
+//        }
+    switch (n){
+        case 0:
+            System.out.println("numarul este zero");
+            break;
+        case 1:
+            System.out.println("numarul este unu");
+            break;
+        case 2:
+            System.out.println("numarul este doi");
+            break;
+        case 3:
+            System.out.println("numarul este trei");
+            break;
+        case 4:
+            System.out.println("numarul este patru");
+            break;
+        case 5:
+            System.out.println("numarul este cinci");
+            break;
+        case 6:
+            System.out.println("numarul este sase");
+            break;
+        case 7:
+            System.out.println("numarul este sapte");
+            break;
+        case 8:
+            System.out.println("numarul este opt");
+            break;
+        case 9:
+            System.out.println("numarul este noua");
+            break;
+        default:
+            System.out.println("nr este gresit");
+
+
+
+
+    }
+    }
+}
