a59f511069b34382c0a414ebe32979fbcd66be52
mrgoggl3s <razvan.jijau@gmail.com>
2019-05-05 18:07:54 +0300

    start ex 19 + 14 intreg

---
+++ src/Ex14.java
@@ -0,0 +1,14 @@
+public class Ex14 {
+    public static void main(String[] args) {
+//        while ( 1 > 0 ) {
+//            System.out.println("going");
+//        }
+//        while ( true ) {
+//            System.out.println("going");
+//        }
+        for (int i = 0; i < 10; System.out.println("second")) {
+            System.out.println("primul afisat");
+        }
+    }
+
+}
---
+++ src/Ex19.java
@@ -0,0 +1,8 @@
+public class Ex19 {
+    public static void main(String[] args) {
+        String cnp = "1881222284550";
+        char sex = cnp.charAt(0);
+        String an = "" + cnp.charAt(1) + cnp.charAt(2);
+
+    }
+}
