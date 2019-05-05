98a31b87611c22555096cec5ef6ee1c24effd009
mrgoggl3s <razvan.jijau@gmail.com>
2019-05-05 13:23:13 +0300

    ex 4

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,2 +1,18 @@
 public class Ex4 {
+    public static void main(String[] args) {
+        int[] nrNat = {4, 6, 7, 5, 45, 768, 4324, 5643};
+        int nrPare = 0;
+        int nrImpare = 0;
+        for (int i = 0; i < nrNat.length; i++) {
+            if (nrNat[i] % 2 == 0) {
+                nrPare = nrPare + 1;
+
+            }
+            else {
+                nrImpare++;
+            }
+        }
+        System.out.println(nrPare);
+        System.out.println(nrImpare);
+    }
 }
